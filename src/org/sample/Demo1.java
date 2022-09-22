2658eb6df2dbedb75f341252b476d2171ba8ebad
siva789 <siva84789@gmail.com>
2022-09-22 11:36:20 +0530

    Boss

---
+++ src/org/sample/Demo1.java
@@ -0,0 +1,14 @@
+package org.sample;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+	
+		int a = 10;
+		int b = 23;
+		int c= a + b;
+		System.out.println(c);
+		System.out.println("Welcome");
+	}
+	
+}
